a9a85b44fef45a428693ab128ec42721d01fc7b3
melvin-viana <mjviana057@gmail.com>
2020-06-25 18:03:35 -0700

    Remove api keys from repo

--- src/app.js
+++ src/app.js
@@ -6,6 +6,8 @@ const initMap = (lat, lng) => {
     zoom: 8
   });
 };
+
+
 // IIFE Invoke func on load
 (async () => {
   try {
@@ -13,20 +15,19 @@ const initMap = (lat, lng) => {
       'https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/search?term=food&latitude=33.8065&longitude=-118.191&',
       {
         headers: {
-          Authorization: `Bearer HHSt-XwlL1oDAhDMxVgFoZRutXcelL20hLoVfUaxh9Xua7rpg8nsUbzZHDVuTaGI8YG9IpKr1lSl9FPFZvM-Ac-Z4rajvEzyQf_cU8RefBmm0BoRDwGs2o2FHerrXnYx`,
+          Authorization: `Bearer ${YELP_API_KEY}`,
           'Access-Control-Allow-Origin': '*',
           'Content-Type': 'application/json'
         }
       }
     );
-    console.log(yelpData);
 
     const {
       data: {
         location: { lat, lng }
       }
     } = await axios.get(
-      'https://geo.ipify.org/api/v1?apiKey=at_j72HRdDtYTOcojOLd6p1j1vK1KDUn',
+      `https://geo.ipify.org/api/v1?apiKey=${GEO_API_KEY}`,
       {
         headers: {
           'Content-Type': 'application/json'
