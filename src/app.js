8127ab52d965f804c3bec9b8fec48d0fbcd8af21
melvin-viana <mjviana057@gmail.com>
2020-06-26 18:38:48 -0700

    Add API key import

--- src/app.js
+++ src/app.js
@@ -1,18 +1,35 @@
 const axios = require('axios');
+const { YELP_API_KEY, GEO_API_KEY } = require('../config');
 
+let map;
 const initMap = (lat, lng) => {
-  const map = new google.maps.Map(document.getElementById('map'), {
+  map = new google.maps.Map(document.getElementById('map'), {
     center: { lat, lng },
     zoom: 8
   });
 };
 
-
 // IIFE Invoke func on load
 (async () => {
   try {
+
+    console.log(yelpData);
+
+    const {
+      data: {
+        location: { lat, lng }
+      }
+    } = await axios.get(
+      `https://geo.ipify.org/api/v1?apiKey=at_${GEO_API_KEY}`,
+      {
+        headers: {
+          'Content-Type': 'application/json'
+        }
+      }
+    );
+
     const yelpData = await axios.get(
-      'https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/search?term=food&latitude=33.8065&longitude=-118.191&',
+      `https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/search?term=food&latitude=${lat}&longitude=${lng}`,
       {
         headers: {
           Authorization: `Bearer ${YELP_API_KEY}`,
@@ -21,19 +38,7 @@ const initMap = (lat, lng) => {
         }
       }
     );
-
-    const {
-      data: {
-        location: { lat, lng }
-      }
-    } = await axios.get(
-      `https://geo.ipify.org/api/v1?apiKey=${GEO_API_KEY}`,
-      {
-        headers: {
-          'Content-Type': 'application/json'
-        }
-      }
-    );
+    console.log(lat, lng);
     // Add map
     initMap(lat, lng);
   } catch (err) {
