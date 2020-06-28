ea039aa9ed6feece7b24604be5dd219b272fa6d0
melvin-viana <mjviana057@gmail.com>
2020-06-27 19:54:41 -0700

    Put init map in helpers module

--- src/app.js
+++ src/app.js
@@ -1,46 +1,12 @@
-const axios = require('axios');
-const { YELP_API_KEY, GEO_API_KEY } = require('../config');
-
-let map;
-const initMap = (lat, lng) => {
-  map = new google.maps.Map(document.getElementById('map'), {
-    center: { lat, lng },
-    zoom: 8
-  });
-};
+const { getLocationData, initMap } = require('./helpers');
 
 // IIFE Invoke func on load
 (async () => {
   try {
-
-    console.log(yelpData);
-
-    const {
-      data: {
-        location: { lat, lng }
-      }
-    } = await axios.get(
-      `https://geo.ipify.org/api/v1?apiKey=at_${GEO_API_KEY}`,
-      {
-        headers: {
-          'Content-Type': 'application/json'
-        }
-      }
-    );
-
-    const yelpData = await axios.get(
-      `https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/search?term=food&latitude=${lat}&longitude=${lng}`,
-      {
-        headers: {
-          Authorization: `Bearer ${YELP_API_KEY}`,
-          'Access-Control-Allow-Origin': '*',
-          'Content-Type': 'application/json'
-        }
-      }
-    );
-    console.log(lat, lng);
+    const { lat, lng } = await getLocationData();
     // Add map
-    initMap(lat, lng);
+    const map = initMap(lat, lng);
+    console.log(map);
   } catch (err) {
     console.error(err);
   }
--- src/helpers/index.js
+++ src/helpers/index.js
@@ -1,22 +1,38 @@
-import 'babel-polyfill';
-
+require('regenerator-runtime/runtime');
 const axios = require('axios');
-const { YELP_API_KEY, GEO_API_KEY } = require('../../config');
+const { GEO_API_KEY } = require('../../config');
+
+/**
+ * @description Get longitude and latitude w/ IP addres from ipify API
+ * @returns {obj} - Latitude and longitude in an object
+ */
+const getLocationData = async () => {
+  const {
+    data: {
+      location: { lat, lng }
+    }
+  } = await axios.get(`https://geo.ipify.org/api/v1?apiKey=at_${GEO_API_KEY}`, {
+    headers: {
+      'Content-Type': 'application/json'
+    }
+  });
+  return { lat, lng };
+};
+
+/**
+ * Create a new google map
+ * @param {number} lat - latitude
+ * @param {number} lng - longitude
+ * @returns
+ */
+const initMap = (lat, lng) => {
+  return new google.maps.Map(document.getElementById('map'), {
+    center: { lat, lng },
+    zoom: 8
+  });
+};
 
 module.exports = {
-  getLocationData: async () => {
-    const {
-      data: {
-        location: { lat, lng }
-      }
-    } = await axios.get(
-      `https://geo.ipify.org/api/v1?apiKey=at_${GEO_API_KEY}`,
-      {
-        headers: {
-          'Content-Type': 'application/json'
-        }
-      }
-    );
-    return { lat, lng };
-  }
+  getLocationData,
+  initMap
 };
