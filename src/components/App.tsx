c342c7ac9013f417190c7d6c8f5c6e3139ed4a0e
melvin-viana <mjviana057@gmail.com>
2020-07-27 16:34:04 -0700

    Delay setRestaurantList call so that marker calls don't cause errors

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,33 +1,40 @@
 import React, { useEffect, useState } from 'react';
-import { getLocationData, initMap, getNearbyEateries, createMarkers, createGoogleScript }  from '../helpers';
-
+import { getLocationData, initMap, getNearbyEateries, createMarkers }  from '../helpers';
+import {RestaurantList} from './RestaurantList';
 interface Props {
   googleApiKey: string;
 }
 
-export const App: React.FC <Props> = ({googleApiKey}) => {
+export const App: React.FC <Props> = () => {
 
-  createGoogleScript(googleApiKey, document);
+
+  const [restaurauntList, setRestaurantList] = useState([]);
+  let delay = ms => new Promise(r => setTimeout(r, ms));
 
   useEffect(() => {
 
-    // IIFE Invoke func on load
-    (async () => {
+    // Fetch data
+    const fetchData = async (queryData: string) => {
       try {
+        //TODO: If queryData is not empty
         const { lat, lng } = await getLocationData();
         // Add map
         const map = initMap(lat, lng);
         const localBusinesses = await getNearbyEateries(lat, lng);
-        createMarkers(localBusinesses, map);
+        const vals = await createMarkers(localBusinesses, map);
+        delay(4100).then(()=>setRestaurantList(localBusinesses))
       } catch (err) {
         console.error(err);
       }
-    })();
+    };
+    // Fetch Data
+    fetchData('');
   },[])
 
   return (
     <React.Fragment>
       <div id="map"></div>
+      <RestaurantList businesses={restaurauntList}/>
     </React.Fragment>
   );
 };
