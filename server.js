74cb7d2f897c01d466570dd1d8b04137174d662e
melvin-viana <mjviana057@gmail.com>
2020-07-29 18:40:26 -0700

    Make 3000 as port and clean server.js

--- server.js
+++ server.js
@@ -1,11 +1,12 @@
 const express = require('express');
 const cors = require('cors');
+
 const app = express();
 
-app.use(cors())
+app.use(cors());
 
-app.use(express.static('client'))
+app.use(express.static('client'));
 
-app.listen(process.env.PORT || 3000, ()=>{
+app.listen(process.env.PORT || 3000, () => {
   console.log('Listening on port 3000');
 });
