ba464f595bdaca0ebe19f6ec90bc7c7bf03186d8
melvin-viana <mjviana057@gmail.com>
2020-08-24 15:57:21 -0700

    Add mongo user model

--- backend/models/User.js
+++ backend/models/User.js
@@ -1,9 +1,37 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
 
-const userSchema = new mongoose.Schema({
+const UserSchema = new mongoose.Schema({
   username: { type: String, unique: true, required: true },
   password: { type: String, required: true },
   restaurants: []
 });
 
-module.exports.User = mongoose.model('USER', userSchema);
+UserSchema.pre('save', function(next) {
+  const user = this;
+  // only hash the password if it has been modified (or is new)
+  if (!user.isModified('password')) return next();
+
+  // generate a salt
+  bcrypt.genSalt(10, function(err, salt) {
+    if (err) return next(err);
+
+    // hash the password along with our new salt
+    bcrypt.hash(user.password, salt, function(err, hash) {
+      if (err) return next(err);
+
+      // override the cleartext password with the hashed one
+      user.password = hash;
+      next();
+    });
+  });
+});
+
+UserSchema.methods.comparePassword = function(candidatePassword, cb) {
+  bcrypt.compare(candidatePassword, this.password, function(err, isMatch) {
+      if (err) return cb(err);
+      cb(null, isMatch);
+  });
+};
+
+module.exports = mongoose.model('User', UserSchema);
