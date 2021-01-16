2bee7e21a52d43128828c8ccc34a73503c1d8794
Melvin Viana <mjviana057@gmail.com>
2021-01-16 15:51:12 -0800

    Use typescript for Express server in backend directory

--- backend/server.ts
+++ backend/server.ts
@@ -1,11 +1,16 @@
 const express = require('express');
 const cors = require('cors');
-
+const path = require('path')
 const app = express();
 
 app.use(cors());
 
 app.use(express.static('client'));
+
+// Handles any requests that don't match the ones above
+app.get('*', (req,res) => {
+  res.sendFile(path.resolve('../client/index.html'));
+});
 
 app.listen(process.env.PORT || 3000, () => {
   console.log('Listening on port 3000');
