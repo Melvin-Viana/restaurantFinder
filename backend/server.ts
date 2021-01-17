289103be3000b2cc253fb51fe1bb4d95f701098c
Melvin Viana <mjviana057@gmail.com>
2021-01-16 17:32:53 -0800

    Fix client path on express server

--- backend/server.ts
+++ backend/server.ts
@@ -8,8 +8,8 @@ app.use(cors());
 app.use(express.static('client'));
 
 // Handles any requests that don't match the ones above
-app.get('*', (req,res) => {
-  res.sendFile(path.resolve('../client/index.html'));
+app.get('*', (req:any, res:any) => {
+  res.sendFile(path.resolve('./client/index.html'));
 });
 
 app.listen(process.env.PORT || 3000, () => {
