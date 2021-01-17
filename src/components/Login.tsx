d5f162256565f55b39fb5d2c450f4f8845b5c077
Melvin Viana <mjviana057@gmail.com>
2021-01-16 17:34:20 -0800

    AddMaterial-UI login tabs

--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -1,5 +1,29 @@
 import React from 'react';
+import {Component, useState} from 'react';
+import {FormControl, FormHelperText, Button, Paper, Tabs, Tab, TextField} from '@material-ui/core';
+import axios from 'axios';
 
-export const Login: React.FC = () => {
-  return (<h2>Hey</h2>)
-};
+export const Login = () => {
+  const [username, setUser] = useState('');
+  const [password, setPassword] = useState('');
+  const [currentForm, setForm] = useState('login')
+  const handleChange = (event: any, value: 'string') =>{setForm(value)}
+  
+  return (<React.Fragment>
+    <Paper square>
+      <Tabs
+        indicatorColor="primary"
+        textColor="primary"
+        aria-label="disabled tabs example"
+        variant="fullWidth"
+        onChange={handleChange}
+        value={currentForm}
+      >
+         <Tab label="Login" value="login"/>
+        <Tab label="SignUp" value="signup" />
+      </Tabs>
+    </Paper>
+  </React.Fragment>);
+  };
+ 
+
