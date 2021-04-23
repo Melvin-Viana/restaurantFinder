2bb374d33a24a42bdd661c6feb7c27ff0b3befb7
Melvin Viana <mjviana057@gmail.com>
2021-04-22 22:57:28 -0700

    Add mongodb connection module

---
+++ backend/dbConnection.ts
@@ -0,0 +1,12 @@
+const db = require('mongoose');
+const mongoURI = process.env.DB_URI ||'mongodb://localhost:27017/restaurantFinder';
+const dbConnection = db.connect(mongoURI, {useNewUrlParser: true});
+
+dbConnection
+  .then(db => console.log(`Connected to: ${mongoURI}`))
+  .catch(err => {
+    console.log(`There was a problem connecting to mongo at: ${mongoURI}`);
+    console.log(err);
+  });
+
+module.exports = dbConnection;
