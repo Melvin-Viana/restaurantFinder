caf25762c5fdd128f0d3b7848190d641336cfaa8
Melvin Viana <mjviana057@gmail.com>
2021-04-22 23:40:04 -0700

    Add routes to login/home and set cookie upon login/signup

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,15 +1,20 @@
-import React, {useState} from 'react';
-import { Route, BrowserRouter, Switch } from 'react-router-dom';
+import React, {useState, useEffect} from 'react';
+import Cookies from 'js-cookie';
+import { Route, BrowserRouter, Switch, Redirect } from 'react-router-dom';
 import {Home} from './Home';
 import {Login} from './Login';
 export const App: React.FC  = () => {
-  const [jwtToken, setJWT] = useState({});
-
+  const [jwToken, setJWT] = useState('');
+  useEffect(() =>{
+    Cookies.set('JWT', jwToken);
+  },[jwToken])
   return (
   <BrowserRouter> 
     <Switch>
-      <Route path="/login" component={Login} />
-      <Route exact path="/" component={Home}/>
+      <Route path="/login">
+      {(jwToken === '')  ? <Login setJWT={setJWT} />: <Redirect to ="/" />} 
+        </Route>
+      <Route exact path="/" component={() => <Home/>}/>
     </Switch>
   </BrowserRouter>
   );
