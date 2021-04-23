447a573cc72df1ec3b93bbc56317010558a027df
Melvin Viana <mjviana057@gmail.com>
2021-04-22 21:27:06 -0700

    Create find user controller

--- backend/controllers/findUser.ts
+++ backend/controllers/findUser.ts
@@ -1,11 +1,13 @@
+
 module.exports = async (UserSchema, username: string, password: string) => {
 
     return UserSchema.findOne({username},  (err: any, user: any) => {
-        if (err) return 'User not found';
+        if (err) throw 'DB ERROR';
     }).then(user =>{
+        if (!user) throw 'User not found';
         const match = user.comparePassword(password, user.password);
         if (match) return user;
-        return 'Password does not match'
+        throw 'Password does not match';
     });
  
 }
