2ed262b098deea5786b81900ad2f0ed90209516a
Melvin Viana <mjviana057@gmail.com>
2021-04-22 23:36:06 -0700

    Create connection to mongoDB server

--- backend/server.ts
+++ backend/server.ts
@@ -1,11 +1,20 @@
 const express = require('express');
 const cors = require('cors');
-const path = require('path')
+const path = require('path');
 const app = express();
+const User = require('./models/User');
+const apiRoutes = require('./routes/apiRoutes')(express, User);
+require('./dbConnection');
+const dotenv = require('dotenv');
+dotenv.config();
 
 app.use(cors());
-
+app.use(express.json());
 app.use(express.static('dist/client'));
+const REACT_APP_URL = process.env.REACT_APP_URL;
+console.log(REACT_APP_URL)
+// API routes here
+app.use('/api', apiRoutes,);
 
 // Handles any requests that don't match the ones above
 app.get('*', (req:any, res:any) => {
