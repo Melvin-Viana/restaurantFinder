f77be77990ee91b22d3555b39dc290070fa582f1
Melvin Viana <mjviana057@gmail.com>
2021-07-23 00:04:08 -0700

    Add see wishlist button

---
+++ src/components/OpenWishList.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import { CSSTransition } from 'react-transition-group'
+
+
+const OpenWishList: React.FC<Props> = () => {
+    return (<div className='wishlist-button'>
+            <button>See Wishlist</button>
+    </div>);
+};
+
+export default OpenWishList
--- src/components/RestaurantList.tsx
+++ src/components/RestaurantList.tsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import Restaurant from './Restaurant';
+import OpenWishList from './OpenWishList';
 import List from '@material-ui/core/List';
 import jwt_decode from 'jwt-decode';
 import Cookies from 'universal-cookie';
@@ -15,12 +16,17 @@ interface decoded {
 }
 export const RestaurantList: React.FC<Props> = ({businesses, restaruantClickHandler, showButtons}) => {
   const [restaurantWishList, setWishlist] = useState([new Set()]);
+  const [loading, setLoading] = useState(false);
   useEffect(()=>{
     const JWT = cookies.get('JWT');
-    const decoded: decoded = jwt_decode(JWT);
-    setWishlist([new Set(decoded.restaurants)]);
-  },[''])
-  return (<List dense={true} className={'.Restaurant-List'}>
+    if(JWT) {
+      const decoded: decoded = jwt_decode(JWT);
+      setWishlist([new Set(decoded.restaurants)]);
+    }
+    if (businesses.length) setLoading(true);
+  },[businesses])
+  return (<List dense={true} className={`Restaurant-List ${!loading && 'long-list'}`}>
     {businesses.map((business, index)=><Restaurant restaurantIndex={index} businessData={business} showButton={restaurantWishList[0].has(business.name) === false && showButtons} restaruantClickHandler={()=> restaruantClickHandler(index)}/>)}
+    {loading &&<OpenWishList/>}
   </List>);
 };
