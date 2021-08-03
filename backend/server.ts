7ca080300c1725fbd2a3eaf647c35e76371909dc
Melvin Viana <mjviana057@gmail.com>
2021-08-03 00:19:45 -0700

    Change server to run public file in its local directory

--- backend/server.ts
+++ backend/server.ts
@@ -9,14 +9,14 @@ const apiRoutes = require('./routes/apiRoutes')(express, User);
 require('./dbConnection');
 app.use(cors());
 app.use(express.json());
-app.use(express.static('dist/client'));
+app.use(express.static('public'));
 
 // API routes here
 app.use('/api', apiRoutes);
 
 // Handles any requests that don't match the ones above
 app.get('*', (req:any, res:any) => {
-  res.sendFile(path.resolve('./dist/client/index.html'));
+  res.sendFile(path.resolve(__dirname, 'public', 'index.html'));
 });
 
 app.listen(process.env.PORT || 3000, () => {
